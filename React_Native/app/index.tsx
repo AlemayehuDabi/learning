dd32037b94edf6c44c560a269ce1ed020fa89ea8
Alemayehu <alemayehudabi606@gmail.com>
2025-07-03 21:05:31 +0300

    lesson_101

---
+++ React_Native/app/index.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.Container}>
+      <Text style={styles.title}>The Number 1</Text>
+      <Text
+        style={{
+          marginTop: 10,
+          marginBottom: 30,
+        }}
+      >
+        Reading List App
+      </Text>
+
+      <View style={styles.card}>
+        <Text>Hello, this is a card</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  Container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  card: {
+    borderRadius: 5,
+    borderColor: "black",
+    backgroundColor: "#eee",
+    boxShadow: "4px  rgba(0,0,0,0.1)",
+    padding: 10,
+  },
+});
